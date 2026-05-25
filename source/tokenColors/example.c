2ae3b200fe57254c2f5eb116ed825435b7b32353
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-25 18:06:54 +0300

    progress example c

--- source/tokenColors/example.c
+++ source/tokenColors/example.c
@@ -17,6 +17,8 @@
 
 SON8_EXTERN_CBEG
 
+static char const *NullData = "\0";
+
 struct son8_c_string {
     char *data;
     size_t size;
@@ -30,6 +32,8 @@ son8_c_string_del( struct son8_c_string *self );
 SON8_EXTERN_CEND
 #endif/*HEADER_H*/
 /* source */
+#include <GLFW/glfw3.h>
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,24 +41,51 @@ SON8_EXTERN_CEND
 
 int
 main( int argc, char *argv[] ) {
+    GLFWwindow *window;
     struct son8_c_string cstr = son8_c_string_new( argv[0] );
+    assert( cstr.data[cstr.size] == '\0' );
     printf( "tokenColors %s\n", cstr.data );
+    if ( !glfwInit(  ) ) return EXIT_FAILURE;
+
+    window = glfwCreateWindow( 640, 360, cstr.data, NULL, NULL );
+
+    if ( !window ) goto terminate;
+
+    glfwMakeContextCurrent( window );
+
+    while ( !glfwWindowShouldClose( window ) ) {
+        glClear( GL_COLOR_BUFFER_BIT );
+        glClearColor( 0.f, 0.f, 0.f, 0.f );
+        glfwSwapBuffers( window );
+        glfwPollEvents( );
+    }
 
     son8_c_string_del( &cstr );
     assert( cstr.data == NULL );
     assert( cstr.size == 0 );
 
     return EXIT_SUCCESS;
+
+terminate:
+    glfwTerminate( );
+    return EXIT_FAILURE;
 }
 
 struct son8_c_string
 son8_c_string_new( char const *str ) {
-    struct son8_c_string cstr;
+    struct son8_c_string ret;
 
-    cstr.size = SON8_CSTRSIZE( str );
-    cstr.data = (char *)malloc( cstr.size );
+    ret.size = SON8_CSTRSIZE( str );
+    ret.data = (char *)malloc( ret.size );
 
-    return cstr;
+    if ( ret.data == NULL ) {
+        ret.size = 1u;
+        str = NullData;
+    }
+
+    memcpy( ret.data, str, ret.size );
+
+    return ret;
 }
 
 void
