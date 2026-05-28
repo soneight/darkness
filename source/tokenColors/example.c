970bf278ec37f8ec2a2161681370dd216f5e4523
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-05-28 05:59:47 +0300

    progress example c

--- source/tokenColors/example.c
+++ source/tokenColors/example.c
@@ -1,3 +1,4 @@
+/* gcc C90 pedantic-errors free example */
 /* header */
 #ifndef HEADER_H
 #define HEADER_H
@@ -12,24 +13,84 @@
 #define SON8_EXTERN_CEND
 #endif/* extern c */
 /* includes */
+#include <limits.h>
 #include <stddef.h>
 #include <stdint.h>
+/* fixed integer types */
+/* char */
+#if CHAR_BIT != 8u && UCHAR_MAX != 0xFFu
+#error "1 byte integer is not supported or exotic architecture"
+#else
+#define SON8INT0 signed char
+#define SON8UNT0 unsigned char
+#define SON8CHAR unsigned char
+#endif
+/* short */
+#if USHRT_MAX == 0xFFFFu
+#define SON8INT1 signed short
+#define SON8UNT1 unsigned short
+#elif UINT_MAX == 0xFFFFu
+#define SON8INT1 signed int
+#define SON8UNT1 unsigned int
+#else
+#error "2 byte integer is not supported or TODO platform specific integer definition"
+#endif
+/* int */
+#if UINT_MAX == 0xFFFFFFFFu
+#define SON8INT2 signed int
+#define SON8UNT2 unsigned int
+#else
+#error "4 byte integer is not supported or TODO platform specific integer definition"
+#endif
+/* long */
+#if ULONG_MAX <= 0xFFFFFFFFul
+#error "8 byte integer is not supported or TODO platform specific integer definition"
+#else
+#define SON8INT3 signed long
+#define SON8UNT3 unsigned long
+#endif
+
+typedef unsigned Son8Bool;
+typedef SON8CHAR Son8Char;
+typedef SON8INT0 Son8Int0;
+typedef SON8UNT0 Son8Unt0;
+typedef SON8INT1 Son8Int1;
+typedef SON8UNT1 Son8Unt1;
+typedef SON8INT2 Son8Int2;
+typedef SON8UNT2 Son8Unt2;
+typedef SON8INT3 Son8Int3;
+typedef SON8UNT3 Son8Unt3;
+typedef char const *Son8CStr;
 
 SON8_EXTERN_CBEG
 
-static char const *NullData = "\0";
-
+static Son8CStr NullData = "\0";
+/* NOTE: size includes null terminated character
+ * - size 0 : error happened (malloc failed?), points to NullData
+ * - size 1 : just empty string, points to NullData
+ * - size 2+: heap allocated string
+ */
 struct Son8String {
     char *data;
     size_t size;
 };
 
 typedef struct Son8String *Son8StringPtr;
+typedef struct Son8String const *Son8StringRef;
+typedef struct Son8String Son8StringVal; /* copy */
 
-struct Son8String
-son8string_new( char const *str );
+Son8StringVal
+son8string_empty( );
+Son8StringVal
+son8string_new( Son8CStr str );
 void
-son8string_del( Son8StringPtr self );
+son8string_del( Son8StringPtr ptr );
+char
+son8string_last( Son8StringVal val );
+Son8Bool
+son8string_valid( Son8StringVal val );
+Son8StringVal
+son8string_copy( Son8StringVal val );
 
 SON8_EXTERN_CEND
 #endif/*HEADER_H*/
@@ -49,7 +110,7 @@ enum Error {
     Error_Last_
 };
 
-static char const *Text_Error[Error_Last_] = {
+static Son8CStr Text_Error[Error_Last_] = {
     "none",
     "arguments count",
     "glwf init",
@@ -60,7 +121,7 @@ int
 main( int argc, char *argv[] ) {
     /* declarations, not mix with code */
     GLFWwindow *window;
-    struct Son8String name;
+    Son8StringVal name, name2;
     size_t error;
     /* code */
     /* checking arguments */
@@ -71,7 +132,7 @@ main( int argc, char *argv[] ) {
     if ( !glfwInit(  ) ) goto error_init_;
     /* creating window */
     name = son8string_new( argv[0] );
-    assert( name.data[name.size] == '\0' );
+    assert( son8string_last( name ) == '\0' );
     window = glfwCreateWindow( 640, 360, name.data, NULL, NULL );
     error = Error_Window;
     if ( !window ) goto error_window_;
@@ -89,7 +150,7 @@ main( int argc, char *argv[] ) {
     }
 
     error = Error_None;
-
+    /* cleaning */
 error_window_:
     son8string_del( &name );
     assert( name.data == NULL );
@@ -99,35 +160,74 @@ error_init_:
 error_argc_:
     assert( error < Error_Last_ );
     if ( error != Error_None ) {
-        puts( "error: " );
-        puts( Text_Error[error] );
-        putchar( '\n' );
+        fprintf( stderr, "error: %s\n", Text_Error[error] );
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
 }
 
-struct Son8String
-son8string_new( char const *cstr ) {
-    struct Son8String result;
+Son8StringVal
+son8string_empty( ) {
+    Son8StringVal empty;
+    empty.data =  (char *)NullData;
+    empty.size = 1u;
+    return empty;
+}
+
+Son8StringVal
+son8string_error( ) {
+    Son8StringVal error;
+    error.data =  (char *)NullData;
+    error.size = 0u;
+    return error;
+}
+
+Son8StringVal
+son8string_new( Son8CStr cstr ) {
+    Son8StringVal result;
+
+    if ( cstr == NULL || cstr[0] == '\0' ) return son8string_empty( );
 
     result.size = SON8_CSTRSIZE( cstr );
     result.data = (char *)malloc( result.size );
 
-    if ( result.data == NULL ) {
-        result.size = 1u;
-        cstr = NullData;
-    }
+    if ( result.data == NULL ) return son8string_error( );
 
     memcpy( result.data, cstr, result.size );
-
     return result;
 }
 
 void
-son8string_del( Son8StringPtr strPtr ) {
-    free( strPtr->data );
+son8string_del( Son8StringPtr ptr ) {
+    if ( ptr->size >= 2 ) free( ptr->data );
+#ifndef NDEBUG /* only clear in debug builds */
+    ptr->data = NULL;
+    ptr->size = 0u;
+#endif
+}
 
-    strPtr->data = NULL;
-    strPtr->size = 0;
+Son8StringVal
+son8string_copy( Son8StringVal val ) {
+    Son8StringVal copy;
+
+    if ( val.size <= 1u ) return val;
+
+    copy.size = val.size;
+    copy.data = (char *)malloc( copy.size );
+
+    if ( copy.data == NULL ) return son8string_error( );
+
+    memcpy( copy.data, val.data, val.size );
+    return copy;
+}
+
+Son8Bool
+son8string_valid( Son8StringVal val )
+{ return val.data != NULL && val.size != 0u; }
+
+char
+son8string_last( Son8StringVal val ) {
+    assert( son8string_valid( val ) );
+
+    return val.data[val.size - 1u];
 }
